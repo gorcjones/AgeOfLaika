2eb7a62ed42868e9c9fa3e962408332a257310f6
rcjones <rcjones.iphone@gmail.com>
2015-05-04 04:36:24 -0500

    Added Real Dog Years

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -33,5 +33,19 @@ class ViewController: UIViewController {
         humanYearsTextField.resignFirstResponder()
     }
 
+    @IBAction func ConvertToRealDogYearsButtonPressed(sender: UIButton) {
+        let humanYearsInt = humanYearsTextField.text.toInt()
+        dogYearsLabel.hidden = false
+        var dogYears:Double
+        if humanYearsInt <= 2{
+            dogYears = Double(humanYearsInt!) * 10.5
+        }else{
+            var x = humanYearsInt! - 2
+            dogYears = (2 * 10.5) + (Double(x) * 4)
+        }
+        humanYearsTextField.text = ""
+        humanYearsTextField.resignFirstResponder()
+        dogYearsLabel.text = "\(dogYears)"
+    }
 }
 
